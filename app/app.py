ce728bd7112d44f4b45ee2c102448a7eb621e5e2
allan raymond <raymonda916@gmail.com>
2026-03-27 09:21:40 +0300

    delete endpoint + authentication

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, File, UploadFile, Form, Depends, HTTPException
-from app.schemas import PostCreate
-from app.db import Post, create_db_and_tables, get_async_session
+from app.schemas import PostCreate, UserCreate, UserRead, UserUpdate
+from app.db import Post, create_db_and_tables, get_async_session, User
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
 from sqlalchemy import select
@@ -10,6 +10,7 @@ import shutil
 import os
 import uuid
 import tempfile
+from app.users import auth_backend, current_active_user, fastapi_users
 
 
 async def lifespan(app: FastAPI):
@@ -17,37 +18,57 @@ async def lifespan(app: FastAPI):
     yield
 app = FastAPI(lifespan=lifespan)
 
+# include all endpoints provided by fastapi_users in the app
+app.include_router(fastapi_users.get_auth_router(
+    auth_backend), prefix='/auth/jwt', tags=["auth"])
+app.include_router(fastapi_users.get_register_router(
+    UserRead, UserCreate), prefix='/auth', tags=["auth"])
+app.include_router(
+    fastapi_users.get_reset_password_router(), prefix='/auth', tags=["auth"])
+app.include_router(fastapi_users.get_verify_router(
+    UserRead), prefix='/auth', tags=["auth"])
+app.include_router(fastapi_users.get_users_router(
+    UserRead, UserUpdate), prefix='/users', tags=["users"])
+# user sends file to our api, which uploads it to imagekit, which then provides a url for the file, which we then server to the user
+
 
 @app.post("/uploads")
 async def upload_file(
     file: UploadFile = File(...),
     caption: str = Form(""),
+    user: User = Depends(current_active_user),
     # -> 'dependency injection'run function, get db object , pass it to us
     session: AsyncSession = Depends(get_async_session)
 ):
     temp_file_path = None
 
     try:
-        with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(file.filename[1])) as temp_file:
+        # created a named temporary file that ends in the extension of the uploaded file
+        with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(file.filename)[1]) as temp_file:
             temp_file_path = temp_file.name
+            # copies content of uploaded file into the temporary file
             shutil.copyfileobj(file.file, temp_file)
 
-            upload_result = imagekit.upload_file(
-                file=open(temp_file_path, "rb"),
-                file_name=file.filename,
-                options=UploadFileRequestOptions(
-                    use_unique_file_name=True,
-                    tags=["backend-upload"]
-                )
+            # upload file to imagekit
+        upload_result = imagekit.upload_file(
+            file=file_bytes,
+            file_name=file.filename,
+            options=UploadFileRequestOptions(
+                use_unique_file_name=True,
+                tags=["backend-upload"]
             )
+        )
 
-        if upload_result.response.http_status_code == 200:
+        # if upload was successful
+        if upload_result.response_metadata.http_status_code == 200:
 
             post = Post(
+                userId=user.id,
                 caption=caption,
-                url="dummyurl",
-                file_type="photo",
-                file_name="dummy name"
+                url=upload_result.url,
+                file_type="video" if file.content_type.startswith(
+                    "video/") else "image",
+                file_name=upload_result.name
             )
 
             session.add(post)
@@ -56,28 +77,63 @@ async def upload_file(
             return post
 
     except Exception as e:
-        pass
+        raise HTTPException(status_code=500, detail=str(e))
 
     finally:
+        # clear up the file object
         if temp_file_path and os.path.exists(temp_file_path):
-            os.unlinck
+            os.unlink(temp_file_path)
 
 
 @app.get("/feed")
-async def get_feed(session: AsyncSession = Depends(get_async_session)):
-    result = await session.execute.select(Post).order_by(Post.created_at.desc())
+async def get_feed(
+        session: AsyncSession = Depends(get_async_session),
+        user: User = Depends(current_active_user)):
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
     posts = [row[0] for row in result.all()]
+
+    result = await session.execute(select(User))
+    users = [row[0] for row in result.all()]
+    user_dict = {u.id: u.email for u in users}
 
     posts_data = []
     for post in posts:
         posts_data.append(
             {
                 "id": str(post.id),
+                "user_id": str(post.user_id),
                 "caption": str(post.caption),
                 "url": post.url,
                 "file_type": post.file_type,
                 "file_name": post.file_name,
-                "created_at": post.created_at.isoformat()
+                "created_at": post.created_at.isoformat(),
+                "is_owner": post.user_id == user.id,
+                "email": user_dict.get(post.user_id, "Unknown")
             }
         )
     return {"posts": posts_data}
+
+
+@app.delete("/posts/{post_id}")
+async def delete_post(post_id: str, session: AsyncSession = Depends(get_async_session), user: User = Depends(current_active_user),):
+
+    try:
+        post_uuid = uuid.UUID(post_id)
+
+        result = await session.execute(select(Post).where(Post.id == post_uuid))
+        post = result.scalars().first()
+
+        if not post:
+            raise HTTPException(status_code=404, detail="Post not found")
+
+        if post.user_id != user.id:
+            raise HTTPException(
+                status_code=403, detail="You don't have the permission to delete this post")
+
+        await session.delete(post)
+        await session.commit()
+
+        return {"success": True, message: {"Post deleted succesfully"}}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
--- app/images.py
+++ app/images.py
@@ -4,10 +4,9 @@ import os
 
 load_dotenv()
 
+
 imagekit = ImageKit(
-    private_key=os.getenv("IMAGEKIT_PRIVATE_KEY"),
     public_key=os.getenv("IMAGEKIT_PUBLIC_KEY"),
+    private_key=os.getenv("IMAGEKIT_PRIVATE_KEY"),
     url_endpoint=os.getenv("IMAGEKIT_URL")
-
-
 )
